55423543c83c3140e8071c791dcc998913de3661
haboh <hhaabboohh@gmail.com>
2019-04-12 10:59:35 +0500

    Map type switcher added.

--- main.py
+++ main.py
@@ -12,23 +12,37 @@ from random import randrange
 from toponym import find_toponym_coordinates, find_district_by_coordinates
 
 MAP_URL = 'https://static-maps.yandex.ru/1.x/'
-FILENAME = "map.png"
+FILENAME = "map"
 LL = [37.620070, 55.753630]
+MAPTYPES = ['map', 'satelite', 'hybrid']
+MAPTYPESSHORT = ['map', 'sat', 'sat,skl']
 
 
 class MapsMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.type = 1
         self.setWindowTitle('Yandex map')
-        self.setGeometry(0, 0, 600, 450)
+        self.setGeometry(0, 0, 700, 450)
         self.pixMap = QLabel(self)
+        self.typeLabel = QLabel(self)
+        self.typeLabel.setText('Map type')
+        self.typeButton = QPushButton(MAPTYPES[self.type], self)
+        self.typeButton.move(600, 30)
+        self.typeLabel.move(625, 0)
+        self.typeButton.clicked.connect(self.change_type)
         self.scale = 3
         self.ll = LL
 
-    def update_map(self, t="map"):
+    def change_type(self):
+        self.type = (self.type + 1) % 3
+        self.typeButton.setText(MAPTYPES[self.type])
+        self.update_map()
+
+    def update_map(self):
         params = {
             "ll": ','.join(map(str, self.ll)),
-            "l": t,
+            "l": MAPTYPESSHORT[self.type],
             "z": str(self.scale),
         }
         response = requests.get(MAP_URL, params=params)
